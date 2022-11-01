53758221bca68ddece6f2105f6fcbc923c84ab75
Michael G. Schwern <schwern@pobox.com>
2022-11-01 16:39:31 -0700

    feature: Generate gem skeleton with Bundler.
    I decided on momento-client. This puts all Momento code in the Momento namespace,
    but it doesn't camp on the momento gem name.

---
+++ lib/momento/client.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "client/version"
+
+module Momento
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/momento/client/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Momento
+  module Client
+    VERSION = "0.1.0"
+  end
+end
