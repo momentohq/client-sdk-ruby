b075c7da9b041fde31677ff4ace7485602dd4ca7
Michael G. Schwern <schwern@pobox.com>
2022-11-08 14:50:40 -0800

    style: Turn off Rubocop warning about empty class.
    Momento::Response is currently empty. It will be filled in once we
    have common things for responses to do.

--- lib/momento/response.rb
+++ lib/momento/response.rb
@@ -4,7 +4,9 @@ require_relative 'response/create_cache'
 require_relative 'response/delete_cache'
 
 module Momento
-  # A superclass for all momento responses.
+  # A superclass for all Momento responses.
+  # rubocop:disable Lint/EmptyClass
   class Response
   end
+  # rubocop:enable Lint/EmptyClass
 end
