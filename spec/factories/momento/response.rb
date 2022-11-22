b48565ea0d7a53bac1eb3225cf807a10ece5c61f
Michael G. Schwern <schwern@pobox.com>
2022-11-21 19:15:16 -0800

    refactor: Remove unused factory traits.

--- spec/factories/momento/response.rb
+++ spec/factories/momento/response.rb
@@ -5,36 +5,8 @@ FactoryBot.define do
     initialize_with { new(**attributes) }
   end
 
-  trait :momento_response_success do
-    initialize_with { new(grpc_response) }
-  end
-
   trait :momento_response_error do
     momento_response
-
-    grpc_exception { GRPC::PermissionDenied.new }
-  end
-
-  trait :momento_response_already_exists do
-    momento_response_error
-
-    grpc_exception { GRPC::AlreadyExists.new }
-  end
-
-  trait :momento_response_invalid_argument do
-    momento_response_error
-
-    grpc_exception { GRPC::InvalidArgument.new }
-  end
-
-  trait :momento_response_not_found do
-    momento_response_error
-
-    grpc_exception { GRPC::NotFound.new }
-  end
-
-  trait :momento_response_permission_denied do
-    momento_response_error
 
     grpc_exception { GRPC::PermissionDenied.new }
   end
