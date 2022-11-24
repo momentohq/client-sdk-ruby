1b140a416cea38426fc0de3ad89c580309f2b708
Michael G. Schwern <schwern@pobox.com>
2022-11-23 16:32:53 -0800

    test: delete_cache live tests.
    The server responds to an invalid cache name inconsistently.
    For create_cache it is an invalid argument. For delete_cache it is
    not found.
    
    We'll have to smooth it out. See #41.

--- spec/live_spec.rb
+++ spec/live_spec.rb
@@ -30,6 +30,67 @@ RSpec.describe 'live acceptance tests', if: ENV.fetch('MOMENTO_TEST_LIVE', nil) 
     client.delete_cache(cache_name)
   end
 
+  shared_context 'when the cache already exists', :include_cache_exists do
+    before {
+      client.create_cache(cache_name)
+    }
+  end
+
+  shared_context 'when the cache does not exist', :include_cache_does_not_exist do
+    # We don't have to do anything, the cache is deleted before each example.
+  end
+
+  shared_context 'when the cache name is empty', :include_empty_cache_name do
+    let(:cache_name) { "" }
+  end
+
+  shared_context 'when the cache name is invalid', :include_invalid_cache_name do
+    let(:cache_name) { "süpé® çåçhê 9ººº" }
+  end
+
+  shared_context 'when the server cannot be contacted', :include_invalid_server do
+    let(:auth_token) {
+      build(:auth_token,
+        c: "thesystemis.down",
+        cp: "thesystemis.down"
+      )
+    }
+  end
+
+  shared_examples 'it handles invalid cache names' do
+    it "responds with InvalidArgumentError", :include_empty_cache_name do
+      is_expected.to have_attributes(
+        success?: false,
+        error?: true,
+        error: have_attributes(
+          error_code: :INVALID_ARGUMENT_ERROR
+        )
+      )
+    end
+
+    it "responds with InvalidArgumentError", :include_invalid_cache_name do
+      is_expected.to have_attributes(
+        success?: false,
+        error?: true,
+        error: have_attributes(
+          error_code: :INVALID_ARGUMENT_ERROR
+        )
+      )
+    end
+  end
+
+  shared_examples 'it handles server failures' do
+    it "responds with SERVER_UNAVAILABLE", :include_invalid_server do
+      is_expected.to have_attributes(
+        success?: false,
+        error?: true,
+        error: have_attributes(
+          error_code: :SERVER_UNAVAILABLE
+        )
+      )
+    end
+  end
+
   # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
   it 'creates, lists, sets, gets, and deletes' do
     key = Faker::Lorem.word
@@ -98,61 +159,37 @@ RSpec.describe 'live acceptance tests', if: ENV.fetch('MOMENTO_TEST_LIVE', nil) 
       client.create_cache(cache_name)
     }
 
-    context 'when the cache already exists' do
-      before {
-        client.create_cache(cache_name)
-      }
-
-      it {
-        is_expected.to have_attributes(
-          success?: false,
-          error?: false,
-          already_exists?: true,
-          error: nil
-        )
-      }
+    it "responds with already exists", :include_cache_exists do
+      is_expected.to have_attributes(
+        success?: false,
+        error?: false,
+        already_exists?: true,
+        error: nil
+      )
     end
 
-    context 'when the cache name is empty' do
-      let(:cache_name) { "" }
+    it_behaves_like 'it handles server failures'
+    it_behaves_like 'it handles invalid cache names'
+  end
 
-      it {
-        is_expected.to have_attributes(
-          success?: false,
-          error?: true,
-          error: have_attributes(
-            error_code: :INVALID_ARGUMENT_ERROR
-          )
+  describe '#delete_cache' do
+    subject {
+      client.delete_cache(cache_name)
+    }
+
+    it "responds with not found", :include_cache_does_not_exist do
+      is_expected.to have_attributes(
+        success?: false,
+        error?: true,
+        error: have_attributes(
+          error_code: :NOT_FOUND_ERROR
         )
-      }
+      )
     end
 
-    context 'when the cache name is invalid' do
-      let(:cache_name) { "süpé® çåçhê 9ººº" }
-
-      it {
-        is_expected.to have_attributes(
-          success?: false,
-          error?: true,
-          error: have_attributes(
-            error_code: :INVALID_ARGUMENT_ERROR
-          )
-        )
-      }
-    end
-
-    context 'when the server cannot be contacted' do
-      let(:auth_token) { build(:auth_token) }
-
-      it {
-        is_expected.to have_attributes(
-          success?: false,
-          error?: true,
-          error: have_attributes(
-            error_code: :SERVER_UNAVAILABLE
-          )
-        )
-      }
+    it_behaves_like 'it handles server failures'
+    skip "Invalid cache name handling is inconsistent" do
+      it_behaves_like 'it handles invalid cache names'
     end
   end
 end
