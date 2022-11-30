0630e15a935f3930f109dfec89549015a1d20547
Michael G. Schwern <schwern@pobox.com>
2022-11-30 15:03:15 -0800

    doc: Document Momento::Error

--- lib/momento/error.rb
+++ lib/momento/error.rb
@@ -2,9 +2,18 @@ require_relative 'error/types'
 
 module Momento
   # Errors from the Momento client or service.
+  # Usually available as `response.error`.
   module Error
-    attr_accessor :cause, :context, :transport_details, :details
+    # @return [Exception] the original exception which was the cause of the error
+    attr_accessor :cause
+    # @return [Hash] any context relevant to the error such as method arguments
+    attr_accessor :context
+    # @return [Momento::Error::TransportDetails] details about the transport layer
+    attr_accessor :transport_details
+    # @return [String] details about the error
+    attr_accessor :details
 
+    # (see #message)
     def to_s
       message
     end
