0f3fc9bf4aaa0c861b5ff69d47390ec2917dae0d
Michael G. Schwern <schwern@pobox.com>
2022-12-01 11:13:35 -0800

    doc: Document Error::TransportDetails and Error::GrpcDetails.

--- lib/momento/error/transport_details.rb
+++ lib/momento/error/transport_details.rb
@@ -1,9 +1,17 @@
 module Momento
   module Error
-    # A class to capture information specific to a particular transport layer.
+    # A class to capture information specific to particular transport layers.
+    #
+    # @example
+    #   # Information about the underlying GRPC error which
+    #   # caused a Momento error response.
+    #   puts response.error.transport_details.grpc.details
     class TransportDetails
+      # Details specific to GRPC.
+      # @return [Momento::Error::GrpcDetails]
       attr_reader :grpc
 
+      # param grpc [GRPC::BadStatus]
       def initialize(grpc:)
         @grpc = GrpcDetails.new(grpc)
       end
