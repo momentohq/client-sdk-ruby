66ca097527cada7a3bb57e8e7836af7b97cd8487
Michael G. Schwern <schwern@pobox.com>
2022-12-01 16:09:44 -0800

    docs: Improve the basic example.
    - No need for the `else` clause for a simple demonstration.
    - Use constants rather than global variables.
    - Show off the success? vs already_exists?

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,45 +1,44 @@
 require 'momento'
 
 # Get your Momento token from an environment variable.
-token = ENV.fetch('MOMENTO_AUTH_TOKEN')
+TOKEN = ENV.fetch('MOMENTO_AUTH_TOKEN')
 
 # Cached items will be deleted after 12.5 seconds.
-ttl = 12.5
+TTL = 12.5
+
+# The name of the cache to create *and delete*
+CACHE_NAME = 'test_cache'.freeze
 
 # Instantiate a Momento client.
 client = Momento::SimpleCacheClient.new(
-  auth_token: token,
-  default_ttl: ttl
+  auth_token: TOKEN,
+  default_ttl: TTL
 )
 
-# Create a cache named "test_cache" to play with.
-response = client.create_cache("test_cache")
-if response.success? || response.already_exists?
-  puts "Created the cache, or it already exists."
+# Create a cache to play with.
+response = client.create_cache(CACHE_NAME)
+if response.success?
+  puts "Created the cache"
+elsif response.already_exists?
+  puts "Cache already exists."
 elsif response.error?
   raise "Couldn't create a cache: #{response.error}"
-else
-  raise
 end
 
 # Put an item in the cache.
-response = client.set("test_cache", "key", "You cached something!")
+response = client.set(CACHE_NAME, "key", "You cached something!")
 if response.success?
   puts "Set an item in the cache."
 elsif response.error?
   raise "Couldn't set an item in the cache: #{response.error}"
-else
-  raise
 end
 
 # And get it back.
-response = client.get("test_cache", "key")
+response = client.get(CACHE_NAME, "key")
 if response.hit?
   puts "Cache returned: #{response.value_string}"
 elsif response.miss?
   puts "The item wasn't found in the cache."
 elsif response.error?
   raise "Couldn't get an item from the cache: #{response.error}"
-else
-  raise
 end
