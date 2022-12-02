692ddf2d41acb641ecbeae649e52d74d794fd11c
Michael G. Schwern <schwern@pobox.com>
2022-12-01 16:33:11 -0800

    docs: Add an example of caching a file.

---
+++ examples/file.rb
@@ -0,0 +1,55 @@
+# This is an example of storing binary data.
+#
+# It will cache a test image, read it back,
+# and write it to disk as test_copy.jpg.
+
+require 'momento'
+
+# Get your Momento token from an environment variable.
+TOKEN = ENV.fetch('MOMENTO_AUTH_TOKEN')
+
+# Cached items will be deleted after 12.5 seconds.
+TTL = 12.5
+
+# The name of the cache to create *and delete*
+CACHE_NAME = 'test_cache'.freeze
+
+FILE_LOCATIONS = [
+  "spec/support/assets/test.jpg",
+  "../spec/support/assets/test.jpg"
+].freeze
+
+# Instantiate a Momento client.
+client = Momento::SimpleCacheClient.new(
+  auth_token: TOKEN,
+  default_ttl: TTL
+)
+
+# Create a cache for testing, or use an already existing one.
+response = client.create_cache(CACHE_NAME)
+raise response.error if response.error?
+
+# Read an image file.
+file = FILE_LOCATIONS.find { |f| File.exist?(f) }
+contents = File.read(file)
+
+# Add the image to the cache.
+puts "Caching #{file}"
+client.set(CACHE_NAME, "test.jpg", contents)
+raise response.error if response.error?
+
+puts "Retrieving the image"
+response = client.get(CACHE_NAME, "test.jpg")
+raise "Cache image was not found!" if response.miss?
+raise response.error if response.error?
+
+puts "Writing the image as test_copy.jpg"
+f = File.open("test_copy.jpg", "wb")
+f.write(response.value_bytes)
+f.close
+
+# Delete our test cache.
+response = client.delete_cache(CACHE_NAME)
+raise response.error if response.error?
+
+puts "Now open test_copy.jpg"
