4baa74e57c7056bc7a6bd1c8298dc87a3a943957
Chris Price <chris@momentohq.com>
2024-06-18 15:22:09 -0700

    feat: add InRegion pre-built config

--- lib/momento/config/configuration.rb
+++ lib/momento/config/configuration.rb
@@ -4,9 +4,16 @@ module Momento
     class Configuration
       attr_reader :transport_strategy
 
-      # Copy constructor; creates a new Configuration with the specified transport strategy
-      def with_transport_strategy(transport_strategy)
-        Configuration.new(transport_strategy)
+      # Convenience function to set the client timeout in milliseconds. If the server has not responded within the
+      # specified time, the client will terminate the request with a DeadlineExceeded error.
+      def with_timeout(timeout_millis)
+        transport_strategy = @transport_strategy
+        grpc_config = transport_strategy.grpc_configuration
+        Configuration.new(
+          transport_strategy.with_grpc_configuration(
+            grpc_config.with_deadline(timeout_millis)
+          )
+        )
       end
 
       # Convenience function to set the number of TCP connections for the client. Each connection can multiplex up
@@ -20,6 +27,11 @@ module Momento
             grpc_config.with_num_grpc_channels(num_connections)
           )
         )
+      end
+
+      # Copy constructor; creates a new Configuration with the specified transport strategy
+      def with_transport_strategy(transport_strategy)
+        Configuration.new(transport_strategy)
       end
 
       def initialize(transport_strategy)
